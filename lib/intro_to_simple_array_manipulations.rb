6d5804e27017c2d9f9f4538bada48740b8678c73
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-02-14 00:59:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,15 +1,11 @@
-def  using_push 
-  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-  next_color = "violet"
-  colors_in_the_rainbow.push(next_color)
+def using_push(arr, str)
+ new_arr =  arr.push(str)
+ 
+ return new_arr
 end
-
-def using_unshift 
-  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
-  @new_neighborhood = "Staten Island"
-  bouroughs_in_nyc.unshift(@new_neighborhood)
-end
-
-
-      
+  
+def using_unshift
+  
+  def
+  
   
